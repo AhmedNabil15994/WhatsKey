a031ef9474189def9ebb3490a9743f0188a32d85
AhmedNabil94 <mody998866@gmail.com>
2022-12-23 07:49:16 +0200

    Messages Actions

--- resources/views/livewire/message-reply.blade.php
+++ resources/views/livewire/message-reply.blade.php
@@ -1,7 +1,7 @@
 <div>
     @php $msg = json_decode(json_encode($msg), true); @endphp
     @if($msg['metadata'] && isset($msg['metadata']['type']) && $msg['metadata']['type'] == 'reply' && isset($msg['metadata']['quotedMessage']))
-    <div class="replyHeader py-3 px-3 d-flex align-items-center mb-1 replyMsg" data-id="{{array_reverse(explode('_',$msg['metadata']['quotedMessageId']))[0]}}" style="min-width: 200px;">
+    <div class="replyHeader mt-3 py-3 px-3 d-flex align-items-center mb-1 replyMsg" data-id="{{array_reverse(explode('_',$msg['metadata']['quotedMessageId']))[0]}}" style="min-width: 200px;">
         <div class="d-flex flex-column align-items-start">
             @if(isset($msg['metadata']['quotedMessage']['fromMe']))
             <span class="text-dark font-weight-bold mb-1">{{$msg['metadata']['quotedMessage']['fromMe'] == 1 ? trans('main.you') : $chatName}}</span>
