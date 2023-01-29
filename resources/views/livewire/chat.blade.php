4fbfdda3fb1320c98a8f54f1120a201ca0146db7
AhmedNabil94 <mody998866@gmail.com>
2023-01-30 01:22:57 +0200

    Fix

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -2,67 +2,69 @@
     @php
         $chat = json_decode(json_encode($chat), true);
         $spanIcon = '';
-        if($chat['lastMessage']['fromMe']){
-            if($chat['lastMessage']['sending_status'] == 1){
-                $spanIcon = '<i class="icon-md la la-check"></i>';
-            }elseif($chat['lastMessage']['sending_status'] == 2){
-                $spanIcon = '<i class="icon-md la la-check-double"></i>';   
-            }elseif($chat['lastMessage']['sending_status'] == 3){
-                $spanIcon = '<i class="icon-md la la-check-double text-primary"></i>';    
-            }elseif($chat['lastMessage']['sending_status'] == 0){
-                $spanIcon = '<i class="icon-md la la-clock"></i>';
-            }
-        }
-
-        $senderText = $chat['lastMessage']['fromMe'] ? ('<span class="fa-icon">'. $spanIcon .'</span>') : '';
         $msgIcon = '';
-        $msgBody = mb_strlen($chat['lastMessage']['body']) > 85 ? mb_substr($chat['lastMessage']['body'], 0, 85) . ' ........' : $chat['lastMessage']['body'];
+        if(isset($chat['lastMessage'])){
+            if($chat['lastMessage']['fromMe']){
+                if($chat['lastMessage']['sending_status'] == 1){
+                    $spanIcon = '<i class="icon-md la la-check"></i>';
+                }elseif($chat['lastMessage']['sending_status'] == 2){
+                    $spanIcon = '<i class="icon-md la la-check-double"></i>';   
+                }elseif($chat['lastMessage']['sending_status'] == 3){
+                    $spanIcon = '<i class="icon-md la la-check-double text-primary"></i>';    
+                }elseif($chat['lastMessage']['sending_status'] == 0){
+                    $spanIcon = '<i class="icon-md la la-clock"></i>';
+                }
+            }
 
-        if($chat['lastMessage']['deleted_at'] != null || $chat['lastMessage']['sending_status'] == 6){
-            $msgBody = trans('main.deletedMsg');
-            $msgIcon = '<span class="fa-icon"><i class="icon-md la la-ban"></i></span>';
-        }else{
-            if($chat['lastMessage']['message_type'] == 'image'){
-                $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.pic');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-camera"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'video'){
-                $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.video');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-video"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'audio'){
-                $msgBody = trans('main.audio');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-headphones"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'document'){
-                $msgBody = mb_strlen($chat['lastMessage']['file_name']) > 85 ? mb_substr($chat['lastMessage']['file_name'], 0, 85) . ' ........' : $chat['lastMessage']['file_name'];
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-file-text"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'location'){
-                $msgBody = $chat['lastMessage']['body'] != '' ? (mb_strlen($chat['lastMessage']['body']) > 85 ? mb_substr($chat['lastMessage']['body'], 0, 85) . ' ........' : $chat['lastMessage']['body']) : trans('main.location');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-map-marker-alt"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'sticker'){
-                $msgBody = trans('main.sticker');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-sticky-note"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'gif'){
-                $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.gif');
-                $msgIcon = '<span class="fa-icon"><svg viewBox="0 0 20 20" width="20" height="20" class=""><path fill="currentColor" d="M4.878 3.9h10.285c1.334 0 1.818.139 2.306.4s.871.644 1.131 1.131c.261.488.4.972.4 2.306v4.351c0 1.334-.139 1.818-.4 2.306a2.717 2.717 0 0 1-1.131 1.131c-.488.261-.972.4-2.306.4H4.878c-1.334 0-1.818-.139-2.306-.4s-.871-.644-1.131-1.131-.4-.972-.4-2.306V7.737c0-1.334.139-1.818.4-2.306s.643-.87 1.131-1.131.972-.4 2.306-.4zm6.193 5.936c-.001-.783.002-1.567-.003-2.35a.597.597 0 0 0-.458-.577.59.59 0 0 0-.683.328.907.907 0 0 0-.062.352c-.004 1.492-.003 2.984-.002 4.476 0 .06.002.121.008.181a.592.592 0 0 0 .468.508c.397.076.728-.196.731-.611.004-.768.001-1.537.001-2.307zm-3.733.687c0 .274-.005.521.002.768.003.093-.031.144-.106.19a2.168 2.168 0 0 1-.905.292c-.819.097-1.572-.333-1.872-1.081a2.213 2.213 0 0 1-.125-1.14 1.76 1.76 0 0 1 1.984-1.513c.359.05.674.194.968.396a.616.616 0 0 0 .513.112.569.569 0 0 0 .448-.464c.055-.273-.055-.484-.278-.637-.791-.545-1.677-.659-2.583-.464-2.006.432-2.816 2.512-2.08 4.196.481 1.101 1.379 1.613 2.546 1.693.793.054 1.523-.148 2.2-.56.265-.161.438-.385.447-.698.014-.522.014-1.045.001-1.568-.007-.297-.235-.549-.51-.557a37.36 37.36 0 0 0-1.64-.001c-.21.004-.394.181-.446.385a.494.494 0 0 0 .217.559.714.714 0 0 0 .313.088c.296.011.592.004.906.004zm6.477-2.519h.171c.811 0 1.623.002 2.434-.001.383-.001.632-.286.577-.654-.041-.274-.281-.455-.611-.455h-3.074c-.474 0-.711.237-.711.713v4.479c0 .243.096.436.306.56.41.241.887-.046.896-.545.009-.504.002-1.008.002-1.511v-.177h.169c.7 0 1.4.001 2.1-.001a.543.543 0 0 0 .535-.388c.071-.235-.001-.488-.213-.611a.87.87 0 0 0-.407-.105c-.667-.01-1.335-.005-2.003-.005h-.172V8.004z"></path></svg></span>';
-            }else if($chat['lastMessage']['message_type'] == 'contact'){
-                $msgBody = trans('main.contact');
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-user-alt"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'list'){
-                $msgBody = (isset($chat['lastMessage']['metadata']['title']) ? $chat['lastMessage']['metadata']['title'] . ' ' : '' ).$chat['lastMessage']['metadata']['body'] . ' ' . $chat['lastMessage']['metadata']['footer'];
-                $msgBody = (mb_strlen($msgBody) > 85 ? mb_mb_substr($msgBody, 0, 85) . ' ........' : $msgBody);
-            }else if($chat['lastMessage']['message_type'] == 'poll'){
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-poll"></i></span>';
-            }else if($chat['lastMessage']['message_type'] == 'product'){
-                $msgIcon = '<span class="fa-icon"><svg width="24" height="24" viewBox="0 0 24 24" class=""><g fill="none" fill-rule="evenodd"><path d="M3.555 5.111h16.888V3H3.555v2.111Zm0 1.057L2.5 11.447v2.111h1.055v6.332H14.11v-6.332h4.224v6.332h2.111v-6.332H21.5v-2.111l-1.055-5.28H3.555ZM5.666 17.78h6.332v-4.223H5.666v4.223Z" id="Page-1-Copy" fill="currentColor"></path></g></svg></span>';
-            }else if($chat['lastMessage']['message_type'] == 'order'){
-                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-shopping-cart"></i></span>';
-                $msgBody = @$chat['lastMessage']['metadata']['itemCount'].' '.trans('main.items');
-            }else if($chat['lastMessage']['message_type'] == 'call'){
-                if($chat['lastMessage']['metadata']['isVideo'] == true){
-                    $msgIcon = '<span class="svg-icon text-danger"><svg viewBox="0 0 19 16" height="16" width="19" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 19 16" xml:space="preserve"><path fill="currentColor" d="M11.2,1.8H3.3C2,1.8,1,2.8,1,4.1V11c0,1.1,1,2.2,2.3,2.2h8c1.2,0,2.3-1,2.3-2.3V4.1 C13.5,2.8,12.4,1.8,11.2,1.8z M10.3,5.3L6.4,9.2h2.1c0.2,0,0.4,0.2,0.4,0.4V10c0,0.2-0.2,0.4-0.4,0.4H4.7c-0.2,0-0.4-0.2-0.4-0.4 V6.2c0-0.2,0.2-0.4,0.4-0.4h0.4c0.2,0,0.4,0.2,0.4,0.4v2.1l3.9-3.9c0.2-0.2,0.4-0.1,0.5,0l0.3,0.3C10.4,4.9,10.4,5.1,10.3,5.3z  M15.1,5.3c-0.2,0.1-0.3,0.3-0.3,0.5v3.3c0,0.2,0.1,0.4,0.3,0.5l2.9,2.1V3.2L15.1,5.3z"></path></svg></span>';
-                    $msgBody = trans('main.missedVideo');
-                }else{
-                    $msgIcon = '<span class="svg-icon text-danger"><svg viewBox="0 0 20 17" height="17" width="20" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 20 17" xml:space="preserve"><path fill="currentColor" d="M18.2,12.1L18.2,12.1c-1.5-1.8-5-2.7-8.2-2.7s-6.7,1-8.2,2.7c-0.7,0.8-0.3,2.3,0.2,2.8 c0.2,0.2,0.3,0.3,0.5,0.3c1.4,0,3.6-0.7,3.6-0.7c0.5-0.2,0.8-0.5,0.8-1c0,0,0-0.7,0-1.3c0.7-1.2,5.4-1.2,6.4-0.1c0,0,0,0,0.1,0.1 v1.3c0,0.2,0.1,0.4,0.2,0.6c0.1,0.2,0.3,0.3,0.5,0.4c0,0,2.2,0.7,3.6,0.7C17.9,15.2,19.1,13.2,18.2,12.1z M5.4,3.2l4.7,4.6l5.8-5.7 L15,1.3L10.1,6L6.4,2.3h2.5V1H4.1v4.8h1.3C5.4,5.8,5.4,3.2,5.4,3.2z"></path></svg></span>';
-                    $msgBody = trans('main.missedVoice');
+            $senderText = $chat['lastMessage']['fromMe'] ? ('<span class="fa-icon">'. $spanIcon .'</span>') : '';
+            $msgBody = mb_strlen($chat['lastMessage']['body']) > 85 ? mb_substr($chat['lastMessage']['body'], 0, 85) . ' ........' : $chat['lastMessage']['body'];
+
+            if($chat['lastMessage']['deleted_at'] != null || $chat['lastMessage']['sending_status'] == 6){
+                $msgBody = trans('main.deletedMsg');
+                $msgIcon = '<span class="fa-icon"><i class="icon-md la la-ban"></i></span>';
+            }else{
+                if($chat['lastMessage']['message_type'] == 'image'){
+                    $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.pic');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-camera"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'video'){
+                    $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.video');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-video"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'audio'){
+                    $msgBody = trans('main.audio');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-headphones"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'document'){
+                    $msgBody = mb_strlen($chat['lastMessage']['file_name']) > 85 ? mb_substr($chat['lastMessage']['file_name'], 0, 85) . ' ........' : $chat['lastMessage']['file_name'];
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-file-text"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'location'){
+                    $msgBody = $chat['lastMessage']['body'] != '' ? (mb_strlen($chat['lastMessage']['body']) > 85 ? mb_substr($chat['lastMessage']['body'], 0, 85) . ' ........' : $chat['lastMessage']['body']) : trans('main.location');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-map-marker-alt"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'sticker'){
+                    $msgBody = trans('main.sticker');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-sticky-note"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'gif'){
+                    $msgBody = $chat['lastMessage']['caption'] != '' ? (mb_strlen($chat['lastMessage']['caption']) > 85 ? mb_substr($chat['lastMessage']['caption'], 0, 85) . ' ........' : $chat['lastMessage']['caption']) : trans('main.gif');
+                    $msgIcon = '<span class="fa-icon"><svg viewBox="0 0 20 20" width="20" height="20" class=""><path fill="currentColor" d="M4.878 3.9h10.285c1.334 0 1.818.139 2.306.4s.871.644 1.131 1.131c.261.488.4.972.4 2.306v4.351c0 1.334-.139 1.818-.4 2.306a2.717 2.717 0 0 1-1.131 1.131c-.488.261-.972.4-2.306.4H4.878c-1.334 0-1.818-.139-2.306-.4s-.871-.644-1.131-1.131-.4-.972-.4-2.306V7.737c0-1.334.139-1.818.4-2.306s.643-.87 1.131-1.131.972-.4 2.306-.4zm6.193 5.936c-.001-.783.002-1.567-.003-2.35a.597.597 0 0 0-.458-.577.59.59 0 0 0-.683.328.907.907 0 0 0-.062.352c-.004 1.492-.003 2.984-.002 4.476 0 .06.002.121.008.181a.592.592 0 0 0 .468.508c.397.076.728-.196.731-.611.004-.768.001-1.537.001-2.307zm-3.733.687c0 .274-.005.521.002.768.003.093-.031.144-.106.19a2.168 2.168 0 0 1-.905.292c-.819.097-1.572-.333-1.872-1.081a2.213 2.213 0 0 1-.125-1.14 1.76 1.76 0 0 1 1.984-1.513c.359.05.674.194.968.396a.616.616 0 0 0 .513.112.569.569 0 0 0 .448-.464c.055-.273-.055-.484-.278-.637-.791-.545-1.677-.659-2.583-.464-2.006.432-2.816 2.512-2.08 4.196.481 1.101 1.379 1.613 2.546 1.693.793.054 1.523-.148 2.2-.56.265-.161.438-.385.447-.698.014-.522.014-1.045.001-1.568-.007-.297-.235-.549-.51-.557a37.36 37.36 0 0 0-1.64-.001c-.21.004-.394.181-.446.385a.494.494 0 0 0 .217.559.714.714 0 0 0 .313.088c.296.011.592.004.906.004zm6.477-2.519h.171c.811 0 1.623.002 2.434-.001.383-.001.632-.286.577-.654-.041-.274-.281-.455-.611-.455h-3.074c-.474 0-.711.237-.711.713v4.479c0 .243.096.436.306.56.41.241.887-.046.896-.545.009-.504.002-1.008.002-1.511v-.177h.169c.7 0 1.4.001 2.1-.001a.543.543 0 0 0 .535-.388c.071-.235-.001-.488-.213-.611a.87.87 0 0 0-.407-.105c-.667-.01-1.335-.005-2.003-.005h-.172V8.004z"></path></svg></span>';
+                }else if($chat['lastMessage']['message_type'] == 'contact'){
+                    $msgBody = trans('main.contact');
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-user-alt"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'list'){
+                    $msgBody = (isset($chat['lastMessage']['metadata']['title']) ? $chat['lastMessage']['metadata']['title'] . ' ' : '' ).$chat['lastMessage']['metadata']['body'] . ' ' . $chat['lastMessage']['metadata']['footer'];
+                    $msgBody = (mb_strlen($msgBody) > 85 ? mb_mb_substr($msgBody, 0, 85) . ' ........' : $msgBody);
+                }else if($chat['lastMessage']['message_type'] == 'poll'){
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-poll"></i></span>';
+                }else if($chat['lastMessage']['message_type'] == 'product'){
+                    $msgIcon = '<span class="fa-icon"><svg width="24" height="24" viewBox="0 0 24 24" class=""><g fill="none" fill-rule="evenodd"><path d="M3.555 5.111h16.888V3H3.555v2.111Zm0 1.057L2.5 11.447v2.111h1.055v6.332H14.11v-6.332h4.224v6.332h2.111v-6.332H21.5v-2.111l-1.055-5.28H3.555ZM5.666 17.78h6.332v-4.223H5.666v4.223Z" id="Page-1-Copy" fill="currentColor"></path></g></svg></span>';
+                }else if($chat['lastMessage']['message_type'] == 'order'){
+                    $msgIcon = '<span class="fa-icon"><i class="icon-md la la-shopping-cart"></i></span>';
+                    $msgBody = @$chat['lastMessage']['metadata']['itemCount'].' '.trans('main.items');
+                }else if($chat['lastMessage']['message_type'] == 'call'){
+                    if($chat['lastMessage']['metadata']['isVideo'] == true){
+                        $msgIcon = '<span class="svg-icon text-danger"><svg viewBox="0 0 19 16" height="16" width="19" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 19 16" xml:space="preserve"><path fill="currentColor" d="M11.2,1.8H3.3C2,1.8,1,2.8,1,4.1V11c0,1.1,1,2.2,2.3,2.2h8c1.2,0,2.3-1,2.3-2.3V4.1 C13.5,2.8,12.4,1.8,11.2,1.8z M10.3,5.3L6.4,9.2h2.1c0.2,0,0.4,0.2,0.4,0.4V10c0,0.2-0.2,0.4-0.4,0.4H4.7c-0.2,0-0.4-0.2-0.4-0.4 V6.2c0-0.2,0.2-0.4,0.4-0.4h0.4c0.2,0,0.4,0.2,0.4,0.4v2.1l3.9-3.9c0.2-0.2,0.4-0.1,0.5,0l0.3,0.3C10.4,4.9,10.4,5.1,10.3,5.3z  M15.1,5.3c-0.2,0.1-0.3,0.3-0.3,0.5v3.3c0,0.2,0.1,0.4,0.3,0.5l2.9,2.1V3.2L15.1,5.3z"></path></svg></span>';
+                        $msgBody = trans('main.missedVideo');
+                    }else{
+                        $msgIcon = '<span class="svg-icon text-danger"><svg viewBox="0 0 20 17" height="17" width="20" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 20 17" xml:space="preserve"><path fill="currentColor" d="M18.2,12.1L18.2,12.1c-1.5-1.8-5-2.7-8.2-2.7s-6.7,1-8.2,2.7c-0.7,0.8-0.3,2.3,0.2,2.8 c0.2,0.2,0.3,0.3,0.5,0.3c1.4,0,3.6-0.7,3.6-0.7c0.5-0.2,0.8-0.5,0.8-1c0,0,0-0.7,0-1.3c0.7-1.2,5.4-1.2,6.4-0.1c0,0,0,0,0.1,0.1 v1.3c0,0.2,0.1,0.4,0.2,0.6c0.1,0.2,0.3,0.3,0.5,0.4c0,0,2.2,0.7,3.6,0.7C17.9,15.2,19.1,13.2,18.2,12.1z M5.4,3.2l4.7,4.6l5.8-5.7 L15,1.3L10.1,6L6.4,2.3h2.5V1H4.1v4.8h1.3C5.4,5.8,5.4,3.2,5.4,3.2z"></path></svg></span>';
+                        $msgBody = trans('main.missedVoice');
+                    }
                 }
             }
         }
