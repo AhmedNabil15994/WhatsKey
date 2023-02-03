a3dc5f64363f5f26dae623cf8b3d97c9efe9281e
AhmedNabil94 <mody998866@gmail.com>
2023-02-03 19:45:33 +0200

    Fix

--- app/Modules/Tenancy/Invoice/Views/invoicePDF.blade.php
+++ app/Modules/Tenancy/Invoice/Views/invoicePDF.blade.php
@@ -14,7 +14,41 @@
 		{{-- <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet"> --}}
 
 		<style>
-			
+			@font-face {
+			    font-family: "Tajawal-Light";
+			    src:url({{config('app.BASE_URL').'/assets/tenant/fonts/Tajawal-Light.ttf'}}) format('truetype');
+			    font-weight: normal;
+			    font-style: normal;
+			}
+
+			@font-face {
+			    font-family: "Tajawal-Regular";
+			    src:url({{config('app.BASE_URL').'/assets/tenant/fonts/Tajawal-Regular.ttf'}}) format('truetype');
+			    font-weight: normal;
+			    font-style: normal;
+			}
+
+			@font-face {
+			    font-family: "Tajawal-Medium";
+			    src:url({{config('app.BASE_URL').'/assets/tenant/fonts/Tajawal-Medium.ttf'}}) format('truetype');
+			    font-weight: normal;
+			    font-style: normal;
+			}
+
+
+			@font-face {
+			    font-family: "Tajawal-Bold";
+			    src: url({{config('app.BASE_URL').'/assets/tenant/fonts/Tajawal-Bold.ttf'}}) format('truetype');
+			    font-weight: normal;
+			    font-style: normal;
+			}
+
+			@font-face {
+			    font-family: "Tajawal-ExtraBold";
+			    src: url({{config('app.BASE_URL').'/assets/tenant/fonts/Tajawal-ExtraBold.ttf'}}) format('truetype');
+			    font-weight: normal;
+			    font-style: normal;
+			}
 		</style>
 
 
