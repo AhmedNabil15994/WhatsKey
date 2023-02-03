adf370e389a75f60c890cb86439ee38100afe153
AhmedNabil94 <mody998866@gmail.com>
2023-02-03 19:26:30 +0200

    Check

--- app/Modules/Tenancy/Invoice/Views/invoicePDF.blade.php
+++ app/Modules/Tenancy/Invoice/Views/invoicePDF.blade.php
@@ -9,7 +9,6 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="stylesheet" href="{{ !isset($data->fontFile) ? asset('assets/tenant/css/font.css') : $data->fontFile }}" />
 		{{-- <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet"> --}}
-		{{dd(asset('assets/tenant/css/font.css'))}}
 		<style type="text/css">
 			html,body{
 				font-family: "Tajawal-Regular" !important;
