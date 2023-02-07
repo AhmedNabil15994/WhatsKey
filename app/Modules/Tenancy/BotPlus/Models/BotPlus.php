6b8ce5de570420c2e53a86faaa29f2f1587d6fc6
AhmedNabil94 <mody998866@gmail.com>
2023-02-07 21:45:18 +0200

    Fix

--- app/Modules/Tenancy/BotPlus/Models/BotPlus.php
+++ app/Modules/Tenancy/BotPlus/Models/BotPlus.php
@@ -36,8 +36,8 @@ class BotPlus extends Model{
             if(!$obj){
                 $allBots = self::NotDeleted()->where('status',1)->where('message_type',2)->search(strtolower($senderMessage))->get();
                 foreach ($allBots as $key => $value) {
-                    Logger(explode(',',$value->message));
-                    if(in_array(strtolower($senderMessage),explode(',',$value->message))){
+                    Logger(array_map('trim', explode(',', $value->message)));
+                    if(in_array(strtolower($senderMessage),array_map('trim', explode(',', $value->message)))){
                         return $value;
                     }
                 }
